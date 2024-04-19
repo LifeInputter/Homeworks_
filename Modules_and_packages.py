c4307f0142d9cc362cbc2fa414771b12612ca0da
LifeInputter <165607918+LifeInputter@users.noreply.github.com>
2024-04-20 00:11:49 +0300

    Add files via upload
    Переделал д/з с учетом замечаний

---
+++ Modules_and_packages.py
@@ -0,0 +1,9 @@
+def print_name():
+    if 1 > 0:
+        print("Hello, world")
+
+
+def say_hello(good_price):
+    good_price >= 10 < 12
+    if good_price >= 10 < 12:
+        print("Hello, world")
